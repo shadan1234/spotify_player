abb103870f22e73a8dd795386aa2d0fb96bf75cb
Shadan <shadan.hussain.2004@gmail.com>
2023-08-01 23:21:07 +0530

    wow

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(Spotify());
 }
 
+class Spotify extends StatelessWidget {
+  const Spotify({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}
 
